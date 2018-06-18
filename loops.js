fbd7d0ff63c5cd4e12dd629045f65d8ffa310c9c
Jonathan Hong <jonhong777@gmail.com>
2018-06-18 21:00:51 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,12 +1,13 @@
+var i
 function forLoop(array){
-  for(var i = 0; i < 25; i++){
-    if(i === 1){
-      array.push("I am 1 strange loop.")
-    }else{
-      array.push("I am ${i} strange loops.")
+  for (let i=0; i<25; i++){
+    if (i=1){
+      array.push("I am 1 stange loop.")
+    } else {
+      array.push('I am ${i} strange loops.')
     }
   }
-  return array
+  return array;
 }
 
 function whileLoop(n){
