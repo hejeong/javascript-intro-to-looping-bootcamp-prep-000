e1faf223683299175d1cd403c69f03d88d15bc72
Jonathan Hong <jonhong777@gmail.com>
2018-06-18 21:01:32 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 var i
 function forLoop(array){
   for (let i=0; i<25; i++){
-    if (i=1){
+    if (i===1){
       array.push("I am 1 stange loop.")
     } else {
       array.push('I am ${i} strange loops.')
